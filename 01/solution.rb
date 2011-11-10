28cda704d5351193d58762479f575997a871434a
dneytcheva <darcho@gbg.bg>
2011-11-11 00:47:17 +0200

    instead of while -> each_cons instead of Hash.new -> = {}

--- 01/solution.rb
+++ 01/solution.rb
@@ -1,21 +1,29 @@
 class Array
+  
   def to_hash
-    {}.tap do |result|
-      each { |key, value| result[key] = value }
-    end
+    new_hash = {}
+    each { |x,y| new_hash[x] = y }
+    new_hash
   end
-
+  
   def index_by
-    map { |n| [yield(n), n] }.to_hash
+    new_hash = {}
+    each { |x| new_hash[yield(x)] = x }
+    new_hash
   end
-
+  
   def subarray_count(subarray)
-    each_cons(subarray.length).count(subarray)
-  end
-
-  def occurences_count
-    Hash.new(0).tap do |result|
-      each { |item| result[item] += 1 }
+    count = 0
+    each_cons(subarray.length) do |sub|
+      sub == subarray ? count += 1 : next
     end
+    count
+  end
+    
+  def occurences_count
+    new_hash = Hash.new(0)
+    each { |x,y| new_hash[x] += 1 }
+    new_hash
   end
 end
+
